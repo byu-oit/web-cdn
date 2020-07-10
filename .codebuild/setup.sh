9d8e202f393983ffee5c4913e4dc228cc351cf11
Blake Morgan <blake_morgan@byu.edu>
2020-07-10 16:26:43 -0600

    Use node 10

--- .codebuild/setup.sh
+++ .codebuild/setup.sh
@@ -8,7 +8,7 @@ apt-get install -yq apt-transport-https
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 
-curl -sL https://deb.nodesource.com/setup_8.x | bash -
+curl -sL https://deb.nodesource.com/setup_10.x | bash -
 
 apt-get update -q && apt-get install -yq nodejs yarn #jq
 
@@ -16,7 +16,7 @@ apt-get update -q && apt-get install -yq nodejs yarn #jq
 
 echo "Installing lerna"
 
-yarn global add lerna@3.1.4
+yarn global add lerna
 
 yarn global add mustache
 
