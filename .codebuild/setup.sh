b2eea0c07e2b38ff7aea119556b7f169240d053c
Glen Sawyer <github@snelg.com>
2023-06-05 15:40:42 -0600

    Still trying to fix GHA

--- .codebuild/setup.sh
+++ .codebuild/setup.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
-echo "Installing Nodejs, Yarn, jq, and yq"
-
-apt-get update -q
-apt-get install -yq apt-transport-https
-
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-
-curl -sL https://deb.nodesource.com/setup_10.x | bash -
-
-apt-get update -q && apt-get install -yq nodejs yarn #jq
-
+#echo "Installing Nodejs, Yarn, jq, and yq"
+#
+#apt-get update -q
+#apt-get install -yq apt-transport-https
+#
+#curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+#echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+#
+#curl -sL https://deb.nodesource.com/setup_10.x | bash -
+#
+#apt-get update -q && apt-get install -yq nodejs yarn #jq
+#
 #pip install yq
+
+npm install -g yarn
 
 echo "Installing lerna"
 
