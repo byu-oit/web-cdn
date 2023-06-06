46f8828275865e6ae5526db5929d35bdcdfd4edf
Glen Sawyer <github@snelg.com>
2023-06-06 10:34:02 -0600

    Fix obsolete aws ECR login syntax

--- assembler/codebuild.sh
+++ assembler/codebuild.sh
@@ -10,7 +10,7 @@ fi
 set -e
 
 echo Logging into ECR
-$(aws ecr get-login --no-include-email --region $AWS_REGION)
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
 image="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/web-community-cdn-assembler:$tag"
 
