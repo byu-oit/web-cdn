a79c22c8b8b59e08e67633cc398b38380dd795d0
ggemre <gmoore1@byu.edu>
2024-04-10 16:05:39 -0600

    chore: tf formatting

--- iac/modules/app/content-bucket.tf
+++ iac/modules/app/content-bucket.tf
@@ -136,8 +136,8 @@ resource "aws_s3_bucket_versioning" "bucket_versioning" {
 
 resource "aws_s3_bucket_policy" "allow_builder_access" {
   depends_on = [aws_s3_bucket_ownership_controls.content_bucket]
-  bucket = aws_s3_bucket.CdnContentBucket.id
-  policy = data.aws_iam_policy_document.CdnContentBucketAllowBuilderUpdates.json
+  bucket     = aws_s3_bucket.CdnContentBucket.id
+  policy     = data.aws_iam_policy_document.CdnContentBucketAllowBuilderUpdates.json
 }
 
 data "aws_iam_policy_document" "CdnContentBucketAllowBuilderUpdates" {
