a9f839ae41e2347b36814492748a37b4a48562e3
Jaden Sterner <92942612+jsterner30@users.noreply.github.com>
2024-04-11 14:46:23 -0600

    fix: api gateways logs

--- iac/modules/app/assemblerTriggerLambda.tf
+++ iac/modules/app/assemblerTriggerLambda.tf
@@ -60,6 +60,17 @@ resource "aws_api_gateway_resource" "proxy" {
   path_part   = "{proxy+}"
 }
 
+resource "aws_api_gateway_method_settings" "logging" {
+  rest_api_id = aws_api_gateway_rest_api.WebHookDomain.id
+  stage_name  = var.env
+  method_path = "*/*"
+
+  settings {
+    metrics_enabled = true
+    logging_level   = "INFO"
+  }
+}
+
 resource "aws_api_gateway_method" "proxy_method" {
   rest_api_id   = aws_api_gateway_rest_api.WebHookDomain.id
   resource_id   = aws_api_gateway_resource.proxy.id
