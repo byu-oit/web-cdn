bb8a4519897030014d8c51ca9aab60ba609ac69c
Caleb Hill <chill389cc@gmail.com>
2024-04-11 14:26:38 -0600

    fix: use lambda arn, not id
    Co-authored-by: Tyler Richey <richeyt@byu.edu>

--- iac/modules/app/log-analyzer-lambda.tf
+++ iac/modules/app/log-analyzer-lambda.tf
@@ -38,7 +38,7 @@ resource "aws_s3_bucket_notification" "LogAnalyzerSorterFuncTrigger" {
   bucket = aws_s3_bucket.LogBucket.id
 
   lambda_function {
-    lambda_function_arn = aws_lambda_function.LogAnalyzerSorterFunc.id
+    lambda_function_arn = aws_lambda_function.LogAnalyzerSorterFunc.arn
     events              = ["s3:ObjectCreated:*"]
     filter_prefix       = local.unprocessed_log_prefix
   }
