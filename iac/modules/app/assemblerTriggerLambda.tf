97f76558fd67c66bcc1d52c8381a955a18041538
Jaden Sterner <92942612+jsterner30@users.noreply.github.com>
2024-04-15 12:53:13 -0600

    fix: path mapping api gateway

--- iac/modules/app/assemblerTriggerLambda.tf
+++ iac/modules/app/assemblerTriggerLambda.tf
@@ -122,7 +122,7 @@ resource "aws_api_gateway_integration" "lambda_integration" {
 
 resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.WebHookDomain.id
-  stage_name  = var.env
+  stage_name  = var.stage_name
   depends_on = [
     aws_api_gateway_integration.lambda_integration,
     aws_api_gateway_resource.proxy,
@@ -155,6 +155,6 @@ resource "aws_route53_record" "webhooks_aaaa_record" {
 
 resource "aws_api_gateway_base_path_mapping" "base_path_mapping" {
   api_id      = aws_api_gateway_rest_api.WebHookDomain.id
-  stage_name  = var.stage_name
+  stage_name  = aws_api_gateway_deployment.deployment.stage_name
   domain_name = aws_api_gateway_domain_name.WebHookDomain.domain_name
 }
