ef16f7c51aa4500dbee0b18def001c2d7b083f4c
Jaden Sterner <92942612+jsterner30@users.noreply.github.com>
2024-04-23 12:53:27 -0600

    feat: cutover dns

--- iac/modules/app/cloudfront.tf
+++ iac/modules/app/cloudfront.tf
@@ -1,35 +1,35 @@
 ## ==================== HTTPS cert ====================
-#resource "aws_acm_certificate" "cert" {
-#  domain_name               = var.cdn_url
-#  validation_method         = "DNS"
-#  subject_alternative_names = ["*.${var.cdn_url}"]
-#}
-#
-#resource "aws_acm_certificate_validation" "cert" {
-#  certificate_arn         = aws_acm_certificate.cert.arn
-#  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
-#}
-#
-#resource "aws_route53_record" "cert_validation" {
-#  for_each = {
-#    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-#      name   = dvo.resource_record_name
-#      record = dvo.resource_record_value
-#      type   = dvo.resource_record_type
-#    }
-#  }
-#
-#  allow_overwrite = true
-#  name            = each.value.name
-#  type            = each.value.type
-#  zone_id         = data.aws_route53_zone.cdn_zone.id
-#  records         = [each.value.record]
-#  ttl             = 60
-#}
+resource "aws_acm_certificate" "cert" {
+  domain_name               = var.cdn_url
+  validation_method         = "DNS"
+  subject_alternative_names = ["*.${var.cdn_url}"]
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.cdn_zone.id
+  records         = [each.value.record]
+  ttl             = 60
+}
 
 # ==================== Route53 ====================
 resource "aws_route53_record" "a_record" {
-  name            = "${local.app_name}-${var.env}"
+  name            = var.cdn_url
   type            = "A"
   zone_id         = data.aws_route53_zone.cdn_zone.id
   allow_overwrite = false
@@ -41,7 +41,7 @@ resource "aws_route53_record" "a_record" {
 }
 
 resource "aws_route53_record" "aaaa_record" {
-  name            = "${local.app_name}-${var.env}"
+  name            = var.cdn_url
   type            = "AAAA"
   zone_id         = data.aws_route53_zone.cdn_zone.id
   allow_overwrite = false
@@ -72,12 +72,12 @@ resource "aws_iam_policy" "allow_cdn_parameter_store_access" {
 
 resource "aws_cloudfront_distribution" "website_cloudfront" {
   comment      = "${var.cdn_url} - ${var.name} ${var.env}"
-  aliases      = ["${local.app_name}.${var.cdn_url}"]
+  aliases      = [var.cdn_url]
   enabled      = true
   http_version = "http2"
 
   viewer_certificate {
-    acm_certificate_arn      = module.acs.certificate_virginia.arn # aws_acm_certificate.new_cert.arn
+    acm_certificate_arn      = aws_acm_certificate.cert.arn
     ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1"
   }
