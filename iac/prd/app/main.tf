4b1213ba1011c51cf4a6b6408f1552f269b82bd8
chlohilt <chloehi@byu.edu>
2024-05-16 13:08:23 -0600

    chore: same changes for prd

--- iac/prd/app/main.tf
+++ iac/prd/app/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "1.4.5"
+  required_version = "1.8.2"
   backend "s3" {
     bucket         = "terraform-state-storage-891377162314"
     dynamodb_table = "terraform-state-lock-891377162314"
@@ -9,7 +9,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.63"
+      version = "~> 5.27"
     }
     local = {
       source  = "hashicorp/local"
